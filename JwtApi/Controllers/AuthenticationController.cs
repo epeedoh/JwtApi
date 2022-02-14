5f24ba6226d021a24c202bc8beb444ae760f1d32
epeedoh <epeedoh@yahoo.fr>
2022-02-14 11:21:57 +0000

    Ajoutez des fichiers projet.

---
+++ JwtApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using JwtApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        public ActionResult Login([FromBody] LoginModel login)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
